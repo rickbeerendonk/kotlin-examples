c90e724089e2bc82542c02e30628319b8a32f770
Rick Beerendonk <rick@beerendonk.com>
2022-01-26 16:30:16 +0100

    Add "Comments" example.

---
+++ 02 - Comments/java/app.kt
@@ -0,0 +1,22 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+fun main() {
+    // One line
+
+    println("1")
+
+    /*
+    Multi
+    line
+    */
+    println("2")
+
+
+    /*
+    Multi
+        /* Nested */
+    line
+    */
+    println("3")
+}
