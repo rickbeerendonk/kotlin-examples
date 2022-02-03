6f8bb73b903583e4340ace7e67409e72170a770a
Rick Beerendonk <rick@beerendonk.com>
2022-02-03 22:25:44 +0100

    Add "Variables - var" example.

---
+++ 03 - Variables/b. var/script/app.kts
@@ -0,0 +1,14 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+// Explicit type
+var explicit: Int = 123;
+
+// Implicit type
+var implicit = 456;
+
+println(explicit::class.simpleName) // Int
+println(implicit::class.simpleName) // Int
+
+// No error (as opposed to val)
+explicit = implicit;
