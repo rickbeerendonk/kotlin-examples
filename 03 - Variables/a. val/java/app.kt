1662d0eb39572cefdc34a402bc48807533b4f753
Rick Beerendonk <rick@beerendonk.com>
2022-02-07 14:16:57 +0100

    Add java version.

---
+++ 03 - Variables/a. val/java/app.kt
@@ -0,0 +1,16 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+fun main() {
+    // Explicit type
+    val explicit: Int = 123;
+
+    // Implicit type
+    val implicit = 456;
+
+    println(explicit::class.simpleName) // Int
+    println(implicit::class.simpleName) // Int
+
+    // error: val cannot be reassigned
+    // explicit = implicit;
+}
