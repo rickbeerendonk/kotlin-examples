6961ec75fd01efbe757cb99e26c24bd8afeb837b
Rick Beerendonk <rick@beerendonk.com>
2022-02-08 14:17:34 +0100

    Clear terminal and show compile...

--- utils/start.sh
+++ utils/start.sh
@@ -1,11 +1,15 @@
-#! /bin/bash -v
+#! /bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 filename=$1
 
+clear
+
 if [[ $filename == *.kts ]]; then
-  sh "$SCRIPT_DIR/../kotlinc/bin/kotlinc" "-script" "$filename"
+    sh "$SCRIPT_DIR/../kotlinc/bin/kotlinc" "-script" "$filename"
 elif [[ $filename == *.kt ]]; then
-  sh "$SCRIPT_DIR/../kotlinc/bin/kotlinc-jvm" "$filename" "-include-runtime" "-d" "temp/app.jar"
-  java -jar temp/app.jar
+    echo Compile...
+    sh "$SCRIPT_DIR/../kotlinc/bin/kotlinc-jvm" "$filename" "-include-runtime" "-d" "temp/app.jar"
+    clear
+    java -jar temp/app.jar
 fi
