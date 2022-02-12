6f1d5eb6223c1370badcb85ca67e6f163f7df620
Rick Beerendonk <rick@beerendonk.com>
2022-02-12 20:32:42 +0100

    Add "Variables - const val" example.

---
+++ 03 - Variables/c. const val/java/app.kt
@@ -0,0 +1,19 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+// Explicit type
+const val explicit: Int = 123;
+
+// Implicit type
+const val implicit = 456;
+
+// error: Only primitives and String are allowed
+//const val iserror = listOf("a", "b");
+
+fun main() {
+    println(explicit::class.simpleName) // Int
+    println(implicit::class.simpleName) // Int
+
+    // error: val cannot be reassigned
+    //explicit = implicit;
+}
