80dd5f66240318b186c0b44ceb3c49e4dc07c31a
Rick Beerendonk <rick@beerendonk.com>
2022-02-15 21:49:03 +0100

    Add "Types - Byte" example.

---
+++ 04 - Types/b. Byte/script/app.kts
@@ -0,0 +1,16 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+// Explicit
+val a: Byte = -128  // Min
+val b: Byte = 127   // Max
+
+println(a::class.simpleName)    // Byte
+println(b::class.qualifiedName) // kotlin.Byte
+
+// Implicit
+val aInt = -128
+val bInt = 127
+
+println(aInt::class.simpleName)    // Int
+println(bInt::class.qualifiedName) // kotlin.Int
