9437a3a35830b187b582902b77f9c5674a0f0f03
Rick Beerendonk <rick@beerendonk.com>
2022-03-04 23:25:40 +0100

    Add "Types - UInt" example.

---
+++ 04 - Types/d2. UInt/script/app.kts
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+println(UInt.MIN_VALUE)  // 0
+println(UInt.MAX_VALUE)  // 4294967295
+
+// Explicit vs Implicit
+val explicit: UInt = 4294967295U
+val implicit = 4294967295U
+
+println(explicit::class.qualifiedName) // kotlin.UInt
+println(implicit::class.qualifiedName) // kotlin.UInt
