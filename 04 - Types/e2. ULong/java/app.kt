f54d5113ce5c7ba409b1bd5596fe97438a5a8229
Rick Beerendonk <rick@beerendonk.com>
2022-03-05 23:28:51 +0100

    Add "Types - ULong" example.

---
+++ 04 - Types/e2. ULong/java/app.kt
@@ -0,0 +1,16 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+fun main() {
+    println(ULong.MIN_VALUE)  // 0
+    println(ULong.MAX_VALUE)  // 18446744073709551615
+
+    // Explicit vs Implicit
+    val explicit: ULong = 1UL
+    val implicit1 = 1U
+    val implicit2 = 1UL
+
+    println(explicit::class.qualifiedName)  // kotlin.ULong
+    println(implicit1::class.qualifiedName) // kotlin.UInt
+    println(implicit2::class.qualifiedName) // kotlin.ULong
+}
