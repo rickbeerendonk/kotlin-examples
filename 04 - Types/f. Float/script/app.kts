39794b2c6e4f8eda436959821c61feb95a5006d5
Rick Beerendonk <rick@beerendonk.com>
2022-03-23 00:08:54 +0100

    Add "Types - Float" example.

---
+++ 04 - Types/f. Float/script/app.kts
@@ -0,0 +1,22 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+// Min and Max
+
+println(Float.MIN_VALUE)  // 1.4E-45
+println(Float.MAX_VALUE)  // 3.4028235E38
+
+// Explicit vs Implicit
+
+val explicit: Float = 1.23F
+val implicit1 = 1.23
+val implicit2 = 1.23F
+
+println(explicit::class.qualifiedName)  // kotlin.Float
+println(implicit1::class.qualifiedName) // kotlin.Double
+println(implicit2::class.qualifiedName) // kotlin.Float
+
+// Literal constants
+
+println(1.23)    //     1.23  (Standard)
+println(1.23e4)  // 12300.0   (Exponential)
