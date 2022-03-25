71a5ecfeeea107f9bf0eaebe188d0e07d4d235b5
Rick Beerendonk <rick@beerendonk.com>
2022-03-26 00:26:16 +0100

    Add "Type - Char" example.

---
+++ 04 - Types/h. Char/java/app.kt
@@ -0,0 +1,8 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+fun main() {
+    val char = 'a'
+    println(char)                       // a
+    println(char::class.qualifiedName)  // kotlin.Char
+}
---
+++ 04 - Types/h. Char/script/app.kts
@@ -0,0 +1,6 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+val char = 'a'
+println(char)                       // a
+println(char::class.qualifiedName)  // kotlin.Char
