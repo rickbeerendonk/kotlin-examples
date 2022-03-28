ad48465850920b74f22f94ad42874a034ac83712
Rick Beerendonk <rick@beerendonk.com>
2022-03-29 01:39:24 +0200

    Add "Packages - Simple" example.

---
+++ 12 - Packages/a. Simple/java/app.kt
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.two
+
+import org.one.*
+
+fun main() {
+    foo()
+}
---
+++ 12 - Packages/a. Simple/java/other.kt
@@ -0,0 +1,8 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.one
+
+fun foo() {
+    println("bar")
+}
