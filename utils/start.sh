0cd795c76d5d16c88361815268dccdafbf41f2f8
Rick Beerendonk <rick@beerendonk.com>
2022-03-31 09:44:16 +0200

    Add colored hints.

--- utils/start.sh
+++ utils/start.sh
@@ -4,12 +4,27 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 FILE=$1
 FILE_DIR=$2
 
+color_off="\033[0m"
+blue="\033[0;34m"
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# Clear terminal
 clear
 
 if [[ $FILE == *.kts ]]; then
+    # Script
+    echo "${blue}${bold}info:${normal}${color_off} script"
+    sh "$SCRIPT_DIR/../kotlinc/bin/kotlinc" -version
+    echo
+
     sh "$SCRIPT_DIR/../kotlinc/bin/kotlinc" "-script" "$FILE"
 elif [[ $FILE == *.kt ]]; then
-    echo Compile...
+    # Java
+    echo "${blue}${bold}info:${normal}${color_off} java"
+    sh "$SCRIPT_DIR/../kotlinc/bin/kotlinc-jvm" -version
+    echo
+
     COMPILED_FILE=temp/app.jar
     sh "$SCRIPT_DIR/../kotlinc/bin/kotlinc-jvm" "$FILE_DIR" "-include-runtime" "-d" "$COMPILED_FILE"
     if [[ -f "$COMPILED_FILE" ]]; then
