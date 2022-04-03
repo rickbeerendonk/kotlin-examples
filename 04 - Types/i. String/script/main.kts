f3bb0e5e6fbb9d23bee15257d192c2c1e4612daf
Rick Beerendonk <rick@beerendonk.com>
2022-04-03 11:02:35 +0200

    Add "Types - String" example.

---
+++ 04 - Types/i. String/script/main.kts
@@ -0,0 +1,11 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+val str = "Text"
+println(str)                       // Text
+println(str.length)                // 4
+println(str.subSequence(1, 3))     // ex
+println(str::class.qualifiedName)  // kotlin.String
+println()
+println(str[0])                       // T
+println(str[0]::class.qualifiedName)  // kotlin.Char
