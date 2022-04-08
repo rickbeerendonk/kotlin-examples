aa37f8cc07d50506dc30d05b6c711007e585463a
Rick Beerendonk <rick@beerendonk.com>
2022-04-08 11:48:28 +0200

    Add "Classes - Methods - Define" example.

---
+++ 13 - Classes/03 - Methods/a. Define/script/main.kts
@@ -0,0 +1,33 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+class Demo {
+    fun nothingExplicit(): Unit {}
+    fun nothingImplicit() {}
+
+    fun somethingExplicit(): String = "something"
+    fun somethingImplicit() = "something"
+
+    fun echo(str: String) = str
+    
+    fun addOne(value: Int) = value + 1
+    
+    fun multipleLine(value: Int): Int {
+        var newValue = value + 1;
+        newValue *= 2;
+        return newValue;
+    }
+}
+
+val demo = Demo()
+println(demo.nothingExplicit())    // kotlin.Unit
+println(demo.nothingImplicit())    // kotlin.Unit
+
+println(demo.somethingExplicit())  // something
+println(demo.somethingImplicit())  // something
+
+println(demo.echo("Echo"))         // Echo
+
+println(demo.addOne(1))            // 2
+
+println(demo.multipleLine(1))      // 4
