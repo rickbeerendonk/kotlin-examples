c67e31f56eacbabc65ad490a039f84cc7248dc0c
Rick Beerendonk <rick@beerendonk.com>
2022-04-17 13:47:46 +0200

    Add "Conversions - toString" example.

---
+++ 05 - Conversions/d. toString/script/main.kts
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+println(false.toString())             // false
+println(Byte.MAX_VALUE.toString())    // 127
+println(Short.MAX_VALUE.toString())   // 32767
+println(Int.MAX_VALUE.toString())     // 2147483647
+println(Long.MAX_VALUE.toString())    // 9223372036854775807
+println(Float.MAX_VALUE.toString())   // 3.4028235E38
+println(Double.MAX_VALUE.toString())  // 1.7976931348623157E308
+println('c'.toString())               // c
+println(Object().toString())          // java.lang.Object@...
