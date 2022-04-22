555d00833a30cebed902fffaf92338bf68f75d58
Rick Beerendonk <rick@beerendonk.com>
2022-04-22 15:34:40 +0200

    Add "Operators - Arithmetic - Double" examples.

---
+++ 06 - Operators/a. Arithmetic/2. Double/java/main.kt
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.operators.arithmetic.double
+
+fun main() {
+    println("Numbers: 7 + 2: ${7.0 + 2.0}"); // 9.0
+    println("Numbers: 7 - 2: ${7.0 - 2.0}"); // 5.0
+    println("Numbers: 7 * 2: ${7.0 * 2.0}"); // 14.0
+    println("Numbers: 7 / 2: ${7.0 / 2.0}"); // 3.5
+    println("Numbers: 7 % 2: ${7.0 % 2.0}"); // 1.0
+}
