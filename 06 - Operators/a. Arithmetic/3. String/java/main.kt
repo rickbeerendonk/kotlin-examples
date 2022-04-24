ec1fa1531fd1e8d3a4d545540313a8c81c20a859
Rick Beerendonk <rick@beerendonk.com>
2022-04-24 15:41:26 +0200

    Add "Operators - Arithmetic - String" examples.

---
+++ 06 - Operators/a. Arithmetic/3. String/java/main.kt
@@ -0,0 +1,9 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.operators.arithmetic.double
+
+fun main() {
+    val str = "a" + "b"
+    println("String: \"a\" + \"b\": ${str}");  // ab
+}
---
+++ 06 - Operators/a. Arithmetic/3. String/script/main.kts
@@ -0,0 +1,5 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+val str = "a" + "b"
+println("String: \"a\" + \"b\": ${str}");  // ab
