5fb1dc3b39744338f0072dcabbf184cd4584fc02
Rick Beerendonk <rick@beerendonk.com>
2022-05-05 16:00:53 +0200

    Add "Operators - Assignment - Double" examples.

---
+++ 06 - Operators/b. Assignment/2. Double/script/main.kts
@@ -0,0 +1,28 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+var a = 7.0
+println("a: ${a}")  // 7.0
+
+a = 2.0
+println("Double: a = 2.0: ${a}") // 2.0
+
+a = 7.0
+a += 2.0
+println("Double: a += 2.0: ${a}") // 9.0
+
+a = 7.0
+a -= 2.0
+println("Double: a -= 2.0: ${a}") // 5.0
+
+a = 7.0
+a *= 2.0
+println("Double: a *= 2.0: ${a}") // 14.0
+
+a = 7.0
+a /= 2.0
+println("Double: a /= 2.0: ${a}") // 3.5
+
+a = 7.0
+a %= 2.0
+println("Double: a %= 2.0: ${a}") // 1.0
