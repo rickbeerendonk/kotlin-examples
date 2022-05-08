9d49356cedcc72b0a27d9e55eddb7ec3c9e0d7ce
Rick Beerendonk <rick@beerendonk.com>
2022-05-08 18:15:34 +0200

    Add "Operators - Logical - And" examples.

---
+++ 06 - Operators/d. Logical/1. And/java/main.kt
@@ -0,0 +1,11 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.operators.logical.and
+
+fun main() {
+    println(true && true)  // true
+    println(true && false)  // false
+    println(false && true)  // false
+    println(false && false)  // false
+}
---
+++ 06 - Operators/d. Logical/1. And/script/main.kts
@@ -0,0 +1,7 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+println(true && true)  // true
+println(true && false)  // false
+println(false && true)  // false
+println(false && false)  // false
