56ea22e60e7f1bc9253f07a980346b0f27cd21f7
Rick Beerendonk <rick@beerendonk.com>
2022-05-10 18:20:20 +0200

    Add "Operators - Logical - Not" examples.

---
+++ 06 - Operators/d. Logical/3. Not/java/main.kt
@@ -0,0 +1,9 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.operators.logical.not
+
+fun main() {
+    println(!true)   // false
+    println(!false)  // true
+}
---
+++ 06 - Operators/d. Logical/3. Not/script/main.kts
@@ -0,0 +1,5 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+println(!true)   // false
+println(!false)  // true
