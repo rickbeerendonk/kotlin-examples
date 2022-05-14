c9b5432b5040d32362929515c02d9685ddef81b7
Rick Beerendonk <rick@beerendonk.com>
2022-05-14 18:44:08 +0200

    Add "Types - String - Templates" examples.

---
+++ 04 - Types/i. String/2 - Templates/jvm/main.kt
@@ -0,0 +1,13 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.types.string.templates
+
+fun main() {
+    val bool = true
+    val number = 123
+    val text = "abc"
+    
+    println("bool: $bool, number: $number, text: $text (length: ${text.length})")
+    // bool: true, number: 123, text: abc (length: 3)
+}
---
+++ 04 - Types/i. String/2 - Templates/script/main.kts
@@ -0,0 +1,9 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+val bool = true
+val number = 123
+val text = "abc"
+
+println("bool: $bool, number: $number, text: $text (length: ${text.length})")
+// bool: true, number: 123, text: abc (length: 3)
