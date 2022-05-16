965ff0559348878da07fb7435e72c71a9fe54c1a
Rick Beerendonk <rick@beerendonk.com>
2022-05-16 18:54:05 +0200

    Add "Conversions - toDouble" examples.

---
+++ 05 - Conversions/d. toDouble/script/main.kts
@@ -0,0 +1,19 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+// Int
+
+// error: The integer literal does not conform to the expected type
+//val d1: Double = 123;
+
+val d2: Double = 123.toDouble()
+println(d2)                      // 123.0
+
+
+// String
+
+println("123.456".toDouble())    // 123.456
+println("1e2".toDouble())        // 100.0
+
+// Exception: java.lang.NumberFormatException
+println("other".toDouble())
