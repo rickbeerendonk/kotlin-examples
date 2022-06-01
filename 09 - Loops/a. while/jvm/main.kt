a0c2de4d9e1ef39a4d66bb69df1073d34ac15674
Rick Beerendonk <rick@beerendonk.com>
2022-06-01 22:27:44 +0200

    Add "Loops - while" example.

---
+++ 09 - Loops/a. while/jvm/main.kt
@@ -0,0 +1,15 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.conditions.while_
+
+fun main() {
+    var i = 1;
+
+    while (i < 5) {
+        println("Iteration $i");
+        i++;
+    }
+
+    println("End")
+}
---
+++ 09 - Loops/a. while/script/main.kts
@@ -0,0 +1,11 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+var i = 1;
+
+while (i < 5) {
+    println("Iteration $i");
+    i++;
+}
+
+println("End")
