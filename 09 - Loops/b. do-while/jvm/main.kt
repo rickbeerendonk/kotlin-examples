d53a24a668665f3f7f3ffa9089b73b9ef58fc4b6
Rick Beerendonk <rick@beerendonk.com>
2022-06-02 22:33:54 +0200

    Add "Loops - do-while" examples.

---
+++ 09 - Loops/b. do-while/jvm/main.kt
@@ -0,0 +1,15 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.conditions.dowhile
+
+fun main() {
+    var i = 10;
+
+    do {
+        println("Iteration $i");
+        i++;
+    } while (i < 5)
+
+    println("End")
+}
