a73bd37613e580f35eb6184bc777f5f861442340
Rick Beerendonk <rick@beerendonk.com>
2022-06-04 23:11:09 +0200

    Add "Loops - for-range" examples.

---
+++ 09 - Loops/d. for-range/jvm/main.kt
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.conditions.forrange
+
+fun main() {
+    for (i in 2..5) println(i)                // 2, 3, 4, 5
+
+    for (i in 1..10 step 3) println(i)        // 1, 4, 7, 10
+
+    for (i in 10 downTo 1 step 3) println(i)  // 10, 7, 4, 1
+}
