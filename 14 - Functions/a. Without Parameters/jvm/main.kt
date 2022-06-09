5947b47d24829755966abe75a35a422a1f35da99
Rick Beerendonk <rick@beerendonk.com>
2022-06-09 23:59:04 +0200

    Add "Functions - Without Parameters" examples.

---
+++ 14 - Functions/a. Without Parameters/jvm/main.kt
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.functions.withoutparameters
+
+fun one() {
+    println("One")
+}
+
+fun main() {
+    one()
+}
