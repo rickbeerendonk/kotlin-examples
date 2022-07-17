4cea2ecad3821c056bd2f56130637f23ff2ea749
Rick Beerendonk <rick@beerendonk.com>
2022-07-17 13:08:18 +0200

    Add "Functions - Local" examples.

---
+++ 14 - Functions/j. Local/jvm/main.kt
@@ -0,0 +1,16 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.functions.local
+
+fun test() {
+    fun local(text: String) {
+        println(text)
+    }
+
+    local("abc");
+}
+
+fun main() {
+    test()
+}
---
+++ 14 - Functions/j. Local/script/main.kts
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+fun test() {
+    fun local(text: String) {
+        println(text)
+    }
+
+    local("abc");
+}
+
+test()
