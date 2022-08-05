1427b174fd55ebefa16a6e9eca83a4d6322ccf04
Rick Beerendonk <rick@beerendonk.com>
2022-08-05 22:21:27 +0200

    Add "Generics - Class" examples.

---
+++ 17 - Generics/a. Class/jvm/main.kt
@@ -0,0 +1,17 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.generics.class_
+
+class Demo<T>(val one: T, val two: T);
+
+fun main() {
+    val demo1 = Demo(1, 2)
+    val demo2 = Demo(3.0, 4.0)
+
+    println("one=${demo1.one}, two=${demo1.two}")
+    println(demo1::class.simpleName)
+    println(demo1.one::class.simpleName)
+
+    println("one=${demo2.one}, two=${demo2.two}")
+}
---
+++ 17 - Generics/a. Class/script/main.kts
@@ -0,0 +1,13 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+class Demo<T>(val one: T, val two: T);
+
+val demo1 = Demo(1, 2)
+val demo2 = Demo(3.0, 4.0)
+
+println("one=${demo1.one}, two=${demo1.two}")
+println(demo1::class.simpleName)
+println(demo1.one::class.simpleName)
+
+println("one=${demo2.one}, two=${demo2.two}")
