8ec3ae82ba6e35de4e3d1104f33f3dabaa26970c
Rick Beerendonk <rick@beerendonk.com>
2022-08-06 22:21:49 +0200

    Add "Generics - Class with Extension Function" examples.

---
+++ 17 - Generics/a. Class with Extension Function/jvm/main.kt
@@ -0,0 +1,14 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.generics.classextensionfun
+
+class Demo<T>(val one: T, val two: T);
+
+// Extension function
+fun <T> Demo<T>.addStr() = "${this.one} + ${this.two}"
+
+fun main() {
+    val demo1 = Demo(1, 2)
+    println(demo1.addStr())
+}
