8428fc54587354066b41c231c3d1b54b300dc136
Rick Beerendonk <rick@beerendonk.com>
2022-08-12 08:47:28 +0200

    Add "Classes - Inner" example.

---
+++ 13 - Classes/07 - Inner/jvm/main.kt
@@ -0,0 +1,26 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.classes.inner_
+
+class Outer(val value: Int) {
+    class Nested() {
+        fun foo(): String {
+            return "Nested: " //+ value
+        }
+    }
+
+   inner class Inner() {
+        fun foo(): String {
+            return "Inner: " + value;
+        }
+    }
+}
+
+fun main() {
+    val nested = Outer.Nested()
+    println(nested.foo())
+
+    val inner_ = Outer(123).Inner()
+    println(inner_.foo())
+}
---
+++ 13 - Classes/07 - Inner/script/main.kts
@@ -0,0 +1,22 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+class Outer(val value: Int) {
+    class Nested() {
+        fun foo(): String {
+            return "Nested: " //+ value
+        }
+    }
+
+   inner class Inner() {
+        fun foo(): String {
+            return "Inner: " + value;
+        }
+    }
+}
+
+val nested = Outer.Nested()
+println(nested.foo())
+
+val inner_ = Outer(123).Inner()
+println(inner_.foo())
