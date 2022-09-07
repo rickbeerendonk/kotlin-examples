e8da8bcb30f7b87c651bffd3591a31d4f3c44fea
Rick Beerendonk <rick@beerendonk.com>
2022-09-07 22:11:12 +0200

    Add "Destructuring - Basics" example.

---
+++ XX - Destructuring/a. Basics/jvm/main.kt
@@ -0,0 +1,16 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.destructuring.basics
+
+class Demo() {
+    operator fun component1() = "one"
+    operator fun component2() = "two"
+}
+
+fun main() {
+    val (first, second) = Demo()
+
+    println(first)   // one
+    println(second)  // two
+}
