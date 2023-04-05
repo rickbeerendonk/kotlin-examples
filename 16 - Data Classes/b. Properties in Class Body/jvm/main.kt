bb07f10f0d1f03ac6b37b74802dc3025859207c4
Rick Beerendonk <rick@beerendonk.com>
2023-04-05 23:55:00 +0200

    Add "Data Classes - Property in Class Body" example.

---
+++ 16 - Data Classes/b. Properties in Class Body/jvm/main.kt
@@ -0,0 +1,26 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+package org.dataclasses.propsinclassbody
+
+data class Demo(val one: Char) {
+    var two: String = ""
+}
+
+fun main() {
+    // Create
+    val demo1 = Demo('1')
+    demo1.two = "A"
+    val demo2 = Demo('1')
+    demo2.two = "B"
+
+    // Supported methods (open):
+    println(demo1.equals(demo2))      // true   <<< FAIL
+    println(demo1.hashCode())         // 49
+    println(demo1.toString())         // Demo(one=1)
+
+    // Supported methods (final):
+    println(demo1.component1())       // 1
+    //println(demo1.component2())       // FAIL
+    //println(demo1.copy(two = "new"))  // FAIL
+}
