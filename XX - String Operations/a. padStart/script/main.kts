aa2d0a45f48c83d7501f7523270ed31cc6877169
Rick Beerendonk <rick@beerendonk.com>
2023-06-19 23:11:13 +0200

    Add "String Operations - padStart" example.

---
+++ XX - String Operations/a. padStart/script/main.kts
@@ -0,0 +1,8 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+println("0".padStart(3))           // '  0'
+
+println("000".padStart(1, 'X'))    // 000
+println("000".padStart(3, 'X'))    // 000
+println("000".padStart(5, 'X'))    // XX000
