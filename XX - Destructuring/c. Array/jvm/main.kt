b4ca1259c155f50e60d8c1854de20b79ee275739
Rick Beerendonk <rick@beerendonk.com>
2023-06-23 22:31:37 +0200

    Add "Destructuring - Array" example.

---
+++ XX - Destructuring/c. Array/jvm/main.kt
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+package org.destructuring.array
+
+fun main() {
+    val (first, second, third) = intArrayOf(1, 2, 3)
+
+    println(first)   // 1
+    println(second)  // 2
+    println(third)   // 3
+}
---
+++ XX - Destructuring/c. Array/script/main.kts
@@ -0,0 +1,8 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+val (first, second, third) = intArrayOf(1, 2, 3)
+
+println(first)   // 1
+println(second)  // 2
+println(third)   // 3
