2bb39780bb46714a18560e1be540fab53d82def2
Rick Beerendonk <rick@beerendonk.com>
2023-08-25 12:28:53 +0200

    Add toBooleanStrict examples.

---
+++ 05 - Conversions/a2. toBooleanStrict/script/main.kts
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+// Strings
+
+println("false".toBooleanStrict())  // false
+println("true".toBooleanStrict())   // true
+
+println("".toBooleanStrict())       // java.lang.IllegalArgumentException: The string doesn't represent a boolean value
+println("other".toBooleanStrict())  // java.lang.IllegalArgumentException: The string doesn't represent a boolean value
