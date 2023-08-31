8208a6e9d2e266a7867b33ba323a2892d3c714f2
Rick Beerendonk <rick@beerendonk.com>
2023-08-31 09:47:19 +0200

    Add "Singleton - Data Object" example.

---
+++ XX - Singleton/b. Data Object/script/main.kts
@@ -0,0 +1,27 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+// No constructor allowed:
+data object Demo1 {
+    val one = 1
+    var two = "two"
+}
+
+data object Demo2 {
+    val one = 1
+    var two = "two"
+}
+
+// Generated Properties
+println(Demo1.one)                // 1
+println(Demo1.two)                // two
+
+// Supported methods (open):
+println(Demo1.equals(Demo2))      // true
+println(Demo1.hashCode())         // 115307
+println(Demo1.toString())         // Demo(one=1 two=two)
+
+// NOT Supported methods (final):
+// Demo1.component1()
+// Demo1.component2()
+// Demo1.copy()
