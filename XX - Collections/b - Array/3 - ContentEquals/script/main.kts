f1117c1fee3cc36576f435935ac3f5b8e6f11dc3
Rick Beerendonk <rick@beerendonk.com>
2023-11-20 23:37:13 +0100

    Add "Collections - Array - ContentEquals" example.

---
+++ XX - Collections/b - Array/3 - ContentEquals/script/main.kts
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+val array1 = arrayOf(1, 2, 3)
+val array2 = arrayOf(1, 2, 3)
+val array3 = arrayOf(3, 2, 1);
+
+println(array1.contentEquals(array2))  // true
+println(array1.contentEquals(array3))  // false
+
+// Infix notation.
+println(array1 contentEquals array2)   // true
