33359138925882a01ce028d7ef6c6baa927edc93
Rick Beerendonk <rick@beerendonk.com>
2023-11-20 23:08:51 +0100

    Add "Operators - Comparison - Greater" example.

---
+++ 06 - Operators/c. Comparison [TODO]/4. Greater Equal/jvm/main.kt
@@ -0,0 +1,19 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+package org.operators.comparison.greaterequal
+
+fun main() {
+    val a = 1
+    val b = 1
+    val c = 2
+
+    // 1 > 1
+    println(a >= b) // true
+
+    // 1 > 2
+    println(a >= c) // false
+
+    // 2 > 1
+    println(c >= a) // true
+}
---
+++ 06 - Operators/c. Comparison [TODO]/4. Greater Equal/script/main.kts
@@ -0,0 +1,15 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+val a = 1
+val b = 1
+val c = 2
+
+// 1 > 1
+println(a >= b) // true
+
+// 1 > 2
+println(a >= c) // false
+
+// 2 > 1
+println(c >= a) // true
