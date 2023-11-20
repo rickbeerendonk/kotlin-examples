188e7ab1060b28f54797f6db77eadd961f5ec919
Rick Beerendonk <rick@beerendonk.com>
2023-11-20 23:12:40 +0100

    Add "Operators - Comparison - Less" example.

---
+++ 06 - Operators/c. Comparison [TODO]/5. Less/script/main.kts
@@ -0,0 +1,15 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+val a = 2;
+val b = 2;
+val c = 1;
+
+// 2 < 2
+println(a < b); // false
+
+// 2 < 1
+println(a < c); // false
+
+// 1 < 2
+println(c < a); // true
