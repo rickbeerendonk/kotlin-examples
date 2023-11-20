8b1699062226be25a84c91ebd38f0d66b3ce761a
Rick Beerendonk <rick@beerendonk.com>
2023-11-20 22:51:21 +0100

    Add "Operators - Comparison - Equality" example.

---
+++ 06 - Operators/c. Comparison [TODO]/1. Equality/jvm/main.kt
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+package org.operators.comparison.equality
+
+fun main() {
+    val n = 1
+    println(n == 1) // true
+    println(n == 2) // false
+}
---
+++ 06 - Operators/c. Comparison [TODO]/1. Equality/script/main.kts
@@ -0,0 +1,6 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+val n = 1
+println(n == 1) // true
+println(n == 2) // false
