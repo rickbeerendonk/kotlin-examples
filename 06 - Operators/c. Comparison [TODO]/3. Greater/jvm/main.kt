0b2fb5e4c28653364a4be2830713ff1c14401a0d
Rick Beerendonk <rick@beerendonk.com>
2023-11-20 23:09:30 +0100

    Prefer val.

--- 06 - Operators/c. Comparison [TODO]/3. Greater/jvm/main.kt
+++ 06 - Operators/c. Comparison [TODO]/3. Greater/jvm/main.kt
@@ -4,9 +4,9 @@
 package org.operators.comparison.greater
 
 fun main() {
-    var a = 1
-    var b = 1
-    var c = 2
+    val a = 1
+    val b = 1
+    val c = 2
 
     // 1 > 1
     println(a > b) // false
--- 06 - Operators/c. Comparison [TODO]/3. Greater/script/main.kts
+++ 06 - Operators/c. Comparison [TODO]/3. Greater/script/main.kts
@@ -1,9 +1,9 @@
 // European Union Public License version 1.2
 // Copyright © 2023 Rick Beerendonk
 
-var a = 1
-var b = 1
-var c = 2
+val a = 1
+val b = 1
+val c = 2
 
 // 1 > 1
 println(a > b) // false
