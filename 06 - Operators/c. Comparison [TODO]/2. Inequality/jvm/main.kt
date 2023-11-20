9950190d4b81c4cc26ca682a40e74486983acc5a
Rick Beerendonk <rick@beerendonk.com>
2023-11-20 22:52:28 +0100

    Add "Operators - Comparison - Inequality" example.

---
+++ 06 - Operators/c. Comparison [TODO]/2. Inequality/jvm/main.kt
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+package org.operators.comparison.inequality
+
+fun main() {
+    val n = 1
+    println(n != 1) // false
+    println(n != 2) // true
+}
