7058507af739f6a52a231c1b9d19404d0cde9ff5
Rick Beerendonk <rick@beerendonk.com>
2023-11-20 23:36:52 +0100

    Add "Operators - Comparison - Less Equal" example.

---
+++ 06 - Operators/c. Comparison [TODO]/6. Less Equal/jvm/main.kt
@@ -0,0 +1,19 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+package org.operators.comparison.lessequal
+
+fun main() {
+    val a = 2;
+    val b = 2;
+    val c = 1;
+
+    // 2 < 2
+    println(a <= b); // true
+
+    // 2 < 1
+    println(a <= c); // false
+
+    // 1 < 2
+    println(c <= a); // true
+}
