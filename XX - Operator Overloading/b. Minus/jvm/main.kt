ab0cee573a4a45c2882ce53de691692aaf9f4ae6
Rick Beerendonk <rick@beerendonk.com>
2023-11-22 09:36:52 +0100

    Add "Operator Overloading - Minus" example.

---
+++ XX - Operator Overloading/b. Minus/jvm/main.kt
@@ -0,0 +1,33 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+package org.operatoroverloading.minus
+
+class MyNumber(var value: Int) {
+
+    operator fun minus(other: MyNumber): MyNumber {
+        return MyNumber(value - other.value)
+    }
+
+    operator fun minus(other: Int): MyNumber {
+        return MyNumber(value - other)
+    }
+
+    fun subtract(other: MyNumber): MyNumber {
+        return this - other
+    }
+
+    override fun toString(): String {
+        return "MyNumber: $value"
+    }
+}
+
+fun main() {
+    val one = MyNumber(1)
+    val two = MyNumber(2)
+
+    println(one)                //  1
+    println(one.subtract(two))  // -1
+    println(one - two)          // -1
+    println(one - 3)            // -2
+}
