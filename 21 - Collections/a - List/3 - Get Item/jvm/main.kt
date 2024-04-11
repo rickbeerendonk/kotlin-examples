b85da3e3c5d74956fe3852fd030cda82dd49ea67
Rick Beerendonk <rick@beerendonk.com>
2024-04-11 11:30:57 +0200

    Add "Collection - List - Get Item" example.

---
+++ 21 - Collections/a - List/3 - Get Item/jvm/main.kt
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+package org.collections.list.size
+
+fun main() {
+    val list = listOf("A", 1, true)
+    println(list[0])  // A
+    println(list[2])  // true
+}
---
+++ 21 - Collections/a - List/3 - Get Item/script/main.kts
@@ -0,0 +1,6 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+val list = listOf("A", 1, true)
+println(list[0])  // A
+println(list[2])  // true
