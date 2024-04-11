971055a6cccacd0ed19c36d2a66e6793c3d6e520
Rick Beerendonk <rick@beerendonk.com>
2024-04-11 09:58:36 +0200

    Add "Lambdas - it" example.

---
+++ 26 - Lambdas/4 - it (one parameter)/script/main.kts
@@ -0,0 +1,22 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+val zero: () -> Unit        = {}
+val one:  (Int) -> Int      = { it }
+val two:  (Int, Int) -> Int = { a, b -> a * b }
+
+println(zero())
+println(one(2))
+println(two(2, 3))
+
+// kotlin.Unit
+// 2
+// 6
+
+fun myPrint(f: (Int) -> (Int)) {
+    println(f(123))
+}
+
+myPrint { 2 * it }
+
+// 246
