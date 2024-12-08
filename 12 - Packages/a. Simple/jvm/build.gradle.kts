1f21eeb5d5dbe48acbb0f5571c4a379a2ffe693c
Rick Beerendonk <rick@beerendonk.com>
2024-12-08 12:54:06 +0100

    Add copyright.

--- 12 - Packages/a. Simple/jvm/build.gradle.kts
+++ 12 - Packages/a. Simple/jvm/build.gradle.kts
@@ -1,9 +1,15 @@
+// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
 plugins {
     kotlin("jvm") version "2.1.0"
 }
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
 }
 
 kotlin {
--- 32 - Coroutines/1 - Simple/jvm/build.gradle.kts
+++ 32 - Coroutines/1 - Simple/jvm/build.gradle.kts
@@ -1,3 +1,6 @@
+// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
 plugins {
     kotlin("jvm") version "2.1.0"
 }
@@ -6,10 +9,10 @@ repositories {
     mavenCentral()
 }
 
-kotlin {
-    jvmToolchain(21)
-}
-
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.9.0")
+}
+
+kotlin {
+    jvmToolchain(21)
 }
