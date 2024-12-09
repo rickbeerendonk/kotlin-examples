b02ff9d69cb59551390d9540cb465bcab7e623e8
Rick Beerendonk <rick@beerendonk.com>
2024-12-10 00:42:03 +0100

    Add "Collections - Set - Size" example.

---
+++ 21 - Collections/c. Set/2 - Size/jvm/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+package org.collections.set.size
+
+fun main() {
+    val s = setOf("A", "B", "C")
+    println(s.size)  // 3
+}
---
+++ 21 - Collections/c. Set/2 - Size/script/main.kts
@@ -0,0 +1,5 @@
+// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+val s = setOf("A", "B", "C")
+println(s.size)  // 3
