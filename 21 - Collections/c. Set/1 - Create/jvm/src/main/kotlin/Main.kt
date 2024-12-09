1c30a1f989f3118e66ad82c765547cdf3b092abc
Rick Beerendonk <rick@beerendonk.com>
2024-12-10 00:35:06 +0100

    Add "Collections - Set - Create" example.

---
+++ 21 - Collections/c. Set/1 - Create/jvm/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+package org.collections.set.create
+
+fun main() {
+    val s = setOf("A", "B", "C")
+
+    s.forEach{ println(it) }
+    // A
+    // B
+    // C
+}
--- utils/start.bat
+++ utils/start.bat
@@ -1,48 +1,67 @@
 @echo off
 
+:: Enable delayed expansion to handle variables within loops or conditions
 setlocal enabledelayedexpansion
 
-set SCRIPT_DIR=%~dp0
-set FILE=%1
-set FILE=%FILE:"=%
-set FILE_DIR=%2
-set FILE_DIR=%FILE_DIR:"=%
+:: Set the script directory
+set "SCRIPT_DIR=%~dp0"
+set "FILE=%~1"
+set "FILE_DIR=%~2"
 
-rem Clear terminal
-cls
+:: Debugging: Echo the received paths
+echo SCRIPT_DIR: "%SCRIPT_DIR%"
+echo FILE: "%FILE%"
+echo FILE_DIR: "%FILE_DIR%"
 
-rem Debug output
-echo SCRIPT_DIR: %SCRIPT_DIR%
-echo FILE: %FILE%
-echo FILE_DIR: %FILE_DIR%
-
-if "%FILE:~-4%" == ".kts" (
-    rem Script
-    echo Running Kotlin script...
-
-    rem Show compiler version
-    call "%SCRIPT_DIR%\kotlinc\bin\kotlinc.bat" -version
-    echo.
-
-    call "%SCRIPT_DIR%\kotlinc\bin\kotlinc.bat" -script "%FILE%"
-) else if "%FILE:~-3%" == ".kt" (
-    rem JVM
-    echo Compiling Kotlin JVM...
-
-    rem Show compiler version
-    call "%SCRIPT_DIR%\kotlinc\bin\kotlinc-jvm.bat" -version
-    echo.
-
-    set COMPILED_FILE=%SCRIPT_DIR%temp\app.jar
-
-    call "%SCRIPT_DIR%\kotlinc\bin\kotlinc-jvm.bat" -cp "%SCRIPT_DIR%\lib\*" "%FILE" -include-runtime -d "%COMPILED_FILE%"
-    if exist "%COMPILED_FILE%" (
-        cls
-        rem Works for coroutine examples (without main in a package):
-        rem java -cp "%SCRIPT_DIR%\lib\*";"%COMPILED_FILE%" MainKt 
-        java -jar "%COMPILED_FILE%"
-        del "%COMPILED_FILE%"
-    )
+:: Check if file and directory exist
+if not exist "%FILE%" (
+    echo Error: The specified file does not exist.
+    exit /b 1
 )
 
-endlocal
+if not exist "%FILE_DIR%" (
+    echo Error: The specified directory does not exist.
+    exit /b 1
+)
+
+:: Clear screen
+cls
+
+:: Check file extension
+if "%FILE:~-4%" == ".kts" (
+    echo [INFO] Script detected
+    echo.
+
+    :: Execute Kotlin script
+    "%SCRIPT_DIR%kotlinc\bin\kotlinc.bat" -script "%FILE%"
+) else if "%FILE:~-3%" == ".kt" (
+    echo [INFO] Kotlin JVM file detected
+    echo.
+
+    :: Compile Kotlin file
+    set "COMPILED_FILE=%SCRIPT_DIR%temp\app.jar"
+    "%SCRIPT_DIR%kotlinc\bin\kotlinc-jvm.bat" -cp "%SCRIPT_DIR%lib\kotlinx-coroutines-core-jvm-1.9.0.jar" "%FILE%" -include-runtime -d "%COMPILED_FILE%"
+    
+    if exist "%COMPILED_FILE%" (
+        :: Automatically detect the main class
+        for /f "tokens=*" %%i in ('jar tf "%COMPILED_FILE%" ^| findstr /r /c:"MainKt\.class$"') do (
+            set "MAIN_CLASS=%%i"
+        )
+        set "MAIN_CLASS=!MAIN_CLASS:.class=!"
+        set "MAIN_CLASS=!MAIN_CLASS:/=.!"
+
+        echo Running main class: !MAIN_CLASS!
+        
+        :: Run the compiled Kotlin program
+        java -cp "%SCRIPT_DIR%lib\*;%COMPILED_FILE%" !MAIN_CLASS!
+        
+        :: Remove the temporary compiled file
+        del "%COMPILED_FILE%"
+    ) else (
+        echo Error: Failed to compile the Kotlin file.
+        exit /b 1
+    )
+) else (
+    echo Error: Unsupported file type. Only .kts and .kt files are supported.
+    exit /b 1
+)
