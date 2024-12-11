1e551effcb6224b1395297c34009cb6e935677a2
Rick Beerendonk <rick@beerendonk.com>
2024-12-11 02:25:39 +0100

    Add "Ranges - DownTo" example.

---
+++ 11 - Ranges [TODO]/c. DownTo/script/main.kts
@@ -0,0 +1,11 @@
+// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+val range = 5 downTo 1
+println(range)
+println(range.toList())
+println(range::class.simpleName)
+
+// 5 downTo 1 setp 1
+// [5, 4, 3, 2, 1]
+// IntProgression
